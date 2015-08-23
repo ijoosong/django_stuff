4c1a117e248af0a19a7db1106e43301e61a6aee8
ijoosong <ijoosong@gmail.com>
2015-08-22 21:44:56 -0400

    myapp

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     'myapp',
-    'south',
 )
 
 MIDDLEWARE_CLASSES = (
