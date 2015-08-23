190fb8bc2643728ca2eed89b283d4d05a354c985
ijoosong <ijoosong@gmail.com>
2015-08-22 21:30:03 -0400

    requirements

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -100,3 +100,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'qwe123',
+    }
+}
